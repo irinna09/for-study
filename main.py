78b8a3ad8f4fe9ef11a01210b38f27beb2368abe
ИРА <ira.paramonova.2000@mail.ru>
2024-03-03 21:27:01 +1000

    init

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def plus(a, b):
+    print(a+b)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +14,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    plus(2, 2)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
